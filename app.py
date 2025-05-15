4e00f0616c057c101264fa872adb0a6290569c2b
Tarciso Costa <150198934+tata2005@users.noreply.github.com>
2025-05-15 15:41:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,4 @@ def listar_produtos():
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5500))  
-    app.run(host='0.0.0.0', port=port) 
+    app.run(host='0.0.0.0', port=5000)
